3f466dd3238be20652dae885f66a158ba4e20bcf
Mark Petruska <petruska.mark@gmail.com>
2018-04-03 11:39:46 +0200

    updates implementation to spec 4.80

--- src/test/scala/com/github/mpetruska/ukmodulo/table/ModulusWeightRowParserSpec.scala
+++ src/test/scala/com/github/mpetruska/ukmodulo/table/ModulusWeightRowParserSpec.scala
@@ -11,11 +11,11 @@ class ModulusWeightRowParserSpec extends WordSpec with Matchers {
     import ModulusWeightRowParser._
 
     "be able to parse the weights table" in {
-      val parseResult = parseAllRows(new InputStreamReader(ModulusWeightRowParser.getClass.getResourceAsStream("/valacdos-v470.txt")))
+      val parseResult = parseAllRows(new InputStreamReader(ModulusWeightRowParser.getClass.getResourceAsStream("/valacdos-v480.txt")))
       parseResult.successful shouldBe true
       
       val rows = parseResult.get
-      rows.length shouldBe 1054
+      rows.length shouldBe 1056
       rows.head   shouldBe ModulusWeightRow("010004", "016715", Mod11, Weights(Vector(0, 0, 0, 0, 0, 0, 8, 7, 6, 5, 4, 3, 2, 1)), None)
       rows.find(_.exceptionCode.isDefined) shouldBe
         Some(ModulusWeightRow("070116", "070116", Mod11, Weights(Vector(0, 0, 7, 6, 5, 8, 9, 4, 5, 6, 7, 8, 9, -1)), Some(12)))
