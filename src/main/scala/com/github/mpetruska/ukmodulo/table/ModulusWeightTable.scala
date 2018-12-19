225932adae5785e910618aa0fea5706b01297805
Mark Petruska <petruska.mark@gmail.com>
2018-12-19 11:00:30 +0100

    implements changes for specification version 5.40

--- src/main/scala/com/github/mpetruska/ukmodulo/table/ModulusWeightTable.scala
+++ src/main/scala/com/github/mpetruska/ukmodulo/table/ModulusWeightTable.scala
@@ -10,7 +10,7 @@ object ModulusWeightTable extends ResourceTable {
 
   type Row = ModulusWeightRow
 
-  val resourcePath = "/valacdos-v530.txt"
+  val resourcePath = "/valacdos-v540.txt"
 
   def parseAllRows(in: Reader): Either[Error, List[Row]] = {
     ModulusWeightRowParser.parseAllRows(in) match {
